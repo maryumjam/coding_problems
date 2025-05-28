2f6461907ddf01141c56b6ee4819ae88331a84e6
maryumja <70666508+maryumja@users.noreply.github.com>
2025-05-28 17:59:38 -0400

    Merging List Problem list

--- MergeSortedLists.py
+++ MergeSortedLists.py
@@ -20,7 +20,7 @@ class ListNode:
 
 
 class Solution:
-    def merge_lists(self, sorted_list1, sorted_list2):
+    def merge_llists(self, sorted_list1, sorted_list2):
         dummy= ListNode(-1)
         current= dummy
         while sorted_list1 and sorted_list2:
@@ -37,15 +37,33 @@ class Solution:
         current.next = sorted_list1 if sorted_list1 else sorted_list2
 
         return dummy.next
+    def merge_list(self, sorted_list1, sorted_list2):
+        i= 0
+        j=0
+        merge_list=[]
+        while i< len(sorted_list1) and j<len(sorted_list2):
+            if sorted_list1[i]<= sorted_list2[j]:
+                merge_list.append(sorted_list1[i])
+                i= i+1
+            else:
+                merge_list.append(sorted_list2[j])
+                j= j+1
+        merge_list.extend(sorted_list1[i:])
+        merge_list.extend(sorted_list2[j:])
+        return merge_list
     
 
 if __name__=='__main__':
-    solution1 = Solution()
+    solution = Solution()
     linkedlist=ListNode()
-    list1= linkedlist.build_linked_list([1,2,3,3,4,5])
-    list2= linkedlist.build_linked_list([1, 3,5,6])
+    llist1= linkedlist.build_linked_list([1,2,3,3,4,5])
+    llist2= linkedlist.build_linked_list([1, 3,5,6])
 
-    linkedlist.print_linked_list(solution1.merge_lists(list1,list2))
+    linkedlist.print_linked_list(solution.merge_llists(llist1,llist2))
+    list1 = [1,2,3,3,4,5]
+    list2 =[1,3,5,6]
+    print(solution.merge_list(list1,list2))
+    
 
 
 
